49d30c8b30c330f134d9591987013d96afe60051
altnbsmehmet <altnbsmehmet@icloud.com>
2025-03-15 17:25:00 +0300

    Responsive design for mobile

--- wwwroot/Scripts/SignInOrUp.js
+++ wwwroot/Scripts/SignInOrUp.js
@@ -1,36 +1,28 @@
 window.addEventListener("DOMContentLoaded", async () => {
 
-    const signInButton0 = document.getElementById('signInButton0');
-    const signInButton1 = document.getElementById('signInButton1');
+    const hamburgerMenu = document.getElementById('hamburgerMenu');
     const signInForm = document.getElementById('signInForm');
-    const signUpButton = document.getElementById('signUpButton');
     const signUpForm = document.getElementById('signUpForm');
-
-    signInButton0.addEventListener('click', function() {
-        if (!signUpForm.classList.contains('hidden')) signUpForm.classList.add('hidden');
-        signInForm.classList.toggle('hidden');
-
-        const buttonRect = signInButton0.getBoundingClientRect();
-        signInForm.style.top = `${buttonRect.bottom + window.scrollY + 5}px`;
-        signInForm.style.left = `${buttonRect.left + window.scrollX - 153}px`;
-    });
-
-    signInButton1.addEventListener('click', function() {
-        if (!signUpForm.classList.contains('hidden')) signUpForm.classList.add('hidden');
-        signInForm.classList.toggle('hidden');
-
-        const buttonRect = signInButton0.getBoundingClientRect();
-        signInForm.style.top = `${buttonRect.bottom + window.scrollY + 5}px`;
-        signInForm.style.left = `${buttonRect.left + window.scrollX - 153}px`;
-    });
-
-    signUpButton.addEventListener('click', function() {
-        if (!signInForm.classList.contains('hidden')) signInForm.classList.add('hidden');
-        signUpForm.classList.toggle('hidden');
-
-        const buttonRect = signInButton0.getBoundingClientRect();
-        signUpForm.style.top = `${buttonRect.bottom + window.scrollY + 5}px`;
-        signUpForm.style.left = `${buttonRect.left + window.scrollX - 208}px`;
+    document.addEventListener('click', (event) => {
+        const buttonType = event.target.dataset.buttonType;
+        if (!buttonType) return;
+        switch (buttonType) {
+            case 'hamburgerMenuButton':
+                if (!signInForm.classList.contains('hidden')) signInForm.classList.add('hidden');
+                if (!signUpForm.classList.contains('hidden')) signUpForm.classList.add('hidden');
+                hamburgerMenu.classList.toggle('hidden');
+            break;
+            case 'signInButton':
+                if (!hamburgerMenu.classList.contains('hidden')) hamburgerMenu.classList.add('hidden');
+                if (!signUpForm.classList.contains('hidden')) signUpForm.classList.add('hidden');
+                signInForm.classList.toggle('hidden');
+            break;
+            case 'signUpButton':
+                if (!hamburgerMenu.classList.contains('hidden')) hamburgerMenu.classList.add('hidden');
+                if (!signInForm.classList.contains('hidden')) signInForm.classList.add('hidden');
+                signUpForm.classList.toggle('hidden');
+            break;
+        }
     });
 
 });
